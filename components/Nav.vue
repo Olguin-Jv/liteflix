4b66b8c0badcf9d33347b64dfc4e8ec5507acbd2
OlguinJv <olguin.j.vito@gmail.com>
2019-09-03 01:10:27 -0300

    basic nav ui

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,18 +1,22 @@
 <template lang='pug'>
   nav
     .container
-      .left
-        a.logo(href='#')
-          img(src='img/liteflix-logo.svg' alt="liteflix logo")
-        .navigation
-          nuxt-link(v-for='section in sections' :key="section.to" :to="section.to" title='title') {{section.name}}
-        .add-wrapp
-          button(@click="click").add-btn
-            span agregar película
-      .right
-        a Niños
-        .bell-wrapp(:class="{active: bell}")
-          .inner
+      a.logo(href='#')
+        img(src='img/liteflix-logo.svg' alt="liteflix logo")
+      .menu
+        .left
+          .navigation
+            nuxt-link(v-for='section in sections' :key="section.to" :to="section.to" title='title') {{section.name}}
+          .add-btn-wrapp
+            button(@click="click").add-btn
+              span agregar película
+        .right
+          a Niños
+          .bell-wrapp(:class="{active: bell}")
+            .inner
+          .dropdown
+            .user-pic
+              img(src='img/profile-placeholder.svg' alt='profile img')
 </template>
 
 <script>
