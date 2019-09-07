18434ed0040bc7d75ae5426ed623ca4d61fb2fbf
OlguinJv <olguin.j.vito@gmail.com>
2019-09-07 18:44:16 -0300

    basic hero ui

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-  nav
+  nav(:class="{'dark-bg': navBg}")
     .container
       a.logo(href='#')
         img(src='img/liteflix-logo.svg' alt="liteflix logo")
@@ -9,25 +9,26 @@
           .navigation
             nuxt-link(v-for='section in sections' :key="section.to" :to="section.to") {{section.name}}
           .add-btn-wrapp
-            button(@click="click").add-btn
+            button(@click="click").btn.add-animated
               span agregar película
 
         .right
           a Niños
           .bell-wrapp(:class="{active: bell}")
             .inner
+
           .menu-dropdown
             .inner(@click="toggleDropdown")
               .user-pic
                 img(src='img/profile-placeholder.svg' alt='profile img')
             .dropdown(:class="{active: menuDropdown}")
-              .user(v-for="user in users" :key="user.name")
+              .user(v-for="user in users" :key="user.name" @click="toggleDropdown")
                 img(:src='user.img' alt='profile img')
                 p {{ user.name }}
               .options
-                nuxt-link(to="configuration") Configuración
-                nuxt-link(to="help") Ayuda
-                nuxt-link(to="/") Log Out
+                nuxt-link(to="configuration" @click.native="toggleDropdown") Configuración
+                nuxt-link(to="help" @click.native="toggleDropdown") Ayuda
+                nuxt-link(to="/" @click.native="toggleDropdown") Log Out
 
 </template>
 
@@ -48,8 +49,15 @@ export default {
         { name: 'User 03', img: 'img/profile-placeholder.svg' }
       ],
       bell: false,
-      menuDropdown: false
+      menuDropdown: false,
+      navBg: false
     }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleNavScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.handleNavScroll)
   },
   methods: {
     click () {
@@ -57,6 +65,9 @@ export default {
     },
     toggleDropdown () {
       this.menuDropdown = !this.menuDropdown
+    },
+    handleNavScroll () {
+      window.scrollY > 50 ? this.navBg = true : this.navBg = false
     }
   }
 }
---
+++ components/landing/Hero.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  section#hero(:style="{'background-image': `url(${heroBg})`}")
+    .container
+      .inner
+        h3
+          | original de&nbsp;
+          span liteflix
+        h1  {{ movie.title }}
+        .wrapp
+          button.btn.play reproducir
+          button.btn.add mi lista
+        h4  Ver temporada 1
+        p  {{ strLimit(210, movie.plot) }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      heroBg: 'https://picsum.photos/1920/1080',
+      movie: {
+        title: 'Kids at school',
+        plot: 'Lorem ipsum dolor amet chicharrones dreamcatcher hammock bushwick hell of, ethical 3 wolf moon celiac neutra mumblecore four dollar toast. Slow-carb post-ironic kickstarter synth franzen.'
+      }
+    }
+  },
+  methods: {
+    strLimit (limit, str) {
+      let txt = ''
+      str.length > limit ? txt = `${str.slice(0, limit)}...` : txt = str
+      return txt
+    }
+  }
+}
+/*
+<div data-content="Some content goes here."></div>
+[data-content]:before {
+content: attr(data-content);
+}
+ */
+</script>
+
+<style lang="scss">
+  @import "~/assets/scss/styles/landing/hero.scss";
+</style>
