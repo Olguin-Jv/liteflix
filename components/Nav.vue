a5c3986c989c390612306bdd74200a2ca04e020d
OlguinJv <olguin.j.vito@gmail.com>
2019-09-10 03:10:25 -0300

    added mobile ui design

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,35 +1,64 @@
 <template lang='pug'>
-  nav(:class="{'dark-bg': navBg}")
-    transition(name="nav")
-      .container(v-if="isLoaded")
-        a.logo(href='#')
-          img(src='img/liteflix-logo.svg' alt="liteflix logo")
+  .nav-wrapper
+    nav(:class="{'dark-bg': navBg}")
+      transition(name="nav")
+        .container(v-if="isLoaded")
+          a.logo(href='#')
+            img(src='img/liteflix-logo.svg' alt="liteflix logo")
 
-        .menu
-          .left
-            .navigation
-              nuxt-link(v-for='section in sections' :key="section.to" :to="section.to" :data-str="section.name") {{section.name}}
-            .add-btn-wrapp
-              button(@click="click").btn.add-animated
-                span agregar película
+          //- DESKTOP VERSION -//
 
-          .right
-            a Niños
-            .bell-wrapp(:class="{active: bell}")
-              .inner
+          .menu
+            .left
+              .navigation
+                nuxt-link(v-for='section in sections.slice(0,4)' :key="section.to" :to="section.to" :data-str="section.name") {{section.name}}
+              .add-btn-wrapp
+                button(@click="click").btn.add-animated
+                  span agregar película
 
-            .menu-dropdown
-              .inner(@click="toggleDropdown")
-                .user-pic
-                  img(src='img/profile-placeholder.svg' alt='profile img')
-              .dropdown(:class="{active: menuDropdown}")
-                .user(v-for="user in users" :key="user.name" @click="toggleDropdown")
-                  img(:src='user.img' alt='profile img')
-                  p {{ user.name }}
-                .options
-                  nuxt-link(to="configuration" @click.native="toggleDropdown") Configuración
-                  nuxt-link(to="help" @click.native="toggleDropdown") Ayuda
-                  nuxt-link(to="/" @click.native="toggleDropdown") Log Out
+            .right
+              a Niños
+              .bell-wrapp(:class="{active: bell}")
+                .inner
+
+              .menu-dropdown
+                .inner(@click="toggleDropdown")
+                  .user-pic
+                    img(src='img/profile-placeholder.svg' alt='profile img')
+                .dropdown(:class="{active: menuDropdown}")
+                  .user(v-for="user in users" :key="user.name" @click="toggleDropdown")
+                    img(:src='user.img' alt='profile img')
+                    p {{ user.name }}
+                  .options
+                    nuxt-link(to="configuration" @click.native="toggleDropdown") Configuración
+                    nuxt-link(to="help" @click.native="toggleDropdown") Ayuda
+                    nuxt-link(to="/" @click.native="toggleDropdown") Log Out
+
+          //- MOBILE VERSION -//
+          .mobile-dropdown
+            button.burger-btn(@click="toggleDropdown")
+              div(:class="{active: menuDropdown}")
+    .dropdown-menu(:class="{active: menuDropdown}")
+      a.logo(href='#')
+        img(src='img/liteflix-logo.svg' alt="liteflix logo")
+
+      .user(@click="toggleDropdown")
+        img(src='img/profile-placeholder.svg' alt='profile img')
+        p {{ users[0].name }}
+      .wrapper.top
+        nuxt-link(to="change-user" @click.native="toggleDropdown") cambia de usuario
+        nuxt-link(to="config" @click.native="toggleDropdown") configuración
+        nuxt-link(to="help" @click.native="toggleDropdown") ayuda
+      .wrapper.bot
+        nuxt-link(to="change-user" @click.native="toggleDropdown")
+          .bell-wrapp(:class="{active: bell}")
+            .inner
+          span novedades
+        nuxt-link(to="change-user" @click.native="toggleDropdown") series
+        nuxt-link(to="change-user" @click.native="toggleDropdown") películas
+        nuxt-link(to="change-user" @click.native="toggleDropdown") mi lista
+        nuxt-link(to="change-user" @click.native="toggleDropdown") niños
+
 </template>
 
 <script>
@@ -42,21 +71,24 @@ export default {
         { name: 'Series', to: 'series' },
         { name: 'Películas', to: 'peliculas' },
         { name: 'Agregados recientemente', to: 'recientes' },
-        { name: 'Mi lista', to: 'mis-listas' }
+        { name: 'Mi lista', to: 'mis-listas' },
+        { name: 'Niños', to: 'ninos' }
       ],
       users: [
         /* hardcoded user list */
-        { name: 'Ernesto Gonzalez', img: 'img/profile-placeholder.svg' },
+        { name: 'Ernesto Garmendia', img: 'img/profile-placeholder.svg' },
         { name: 'User 02', img: 'img/profile-placeholder.svg' },
         { name: 'User 03', img: 'img/profile-placeholder.svg' }
       ],
       bell: false,
       menuDropdown: false,
       navBg: false,
-      isLoaded: false
+      isLoaded: false,
+      isMobile: false
     }
   },
   mounted () {
+    this.handleNavScroll()
     window.addEventListener('scroll', this.handleNavScroll)
     this.isLoaded = true
   },
