dfc1a2a12488e46ae0c8f97b552e6e464cbd7c8d
OlguinJv <olguin.j.vito@gmail.com>
2019-09-13 03:10:20 -0300

    basic upload

--- components/AddMovies.vue
+++ components/AddMovies.vue
@@ -7,9 +7,17 @@
           span Agregar archivo&nbsp;
           | o arrastrarlo y soltarlo aquí
       .input-area
-        input
-        .drop
-          button(@click="addMovie") add movie
+        .wrapp
+          p nombre de la pelicula
+          input(v-model="movieName")
+        .wrapp
+          p categoría
+          .select(@click="selectActive = !selectActive")
+            p {{ movieGenre }}
+            .select__dropdown(:class="{'active': selectActive}")
+              .inner
+                .option(v-for="(categorie, index) in categories" :key="index" @click="movieGenre = categorie") {{ categorie }}
+      button.btn-upload(@click="addMovie") Subir Película
 </template>
 
 <script>
@@ -17,7 +25,11 @@ export default {
   name: 'AddMovies',
   data () {
     return {
-      data: { title: 'batman', caaategorie: 'berretas' }
+      data: {},
+      movieName: 'Ingresa el nombre',
+      movieGenre: 'Selecciona categoría',
+      categories: ['accion', 'animacion', 'aventuras', 'ciencia ficcion', 'comedia', 'documentales', 'drama', 'épicas', 'terror'],
+      selectActive: false
     }
   },
   methods: {
@@ -25,7 +37,14 @@ export default {
       this.$bus.$emit('toggle-add-movie')
     },
     addMovie () {
+      this.data.title = this.movieName
+      this.data.genre = this.movieGenre
       this.$bus.$emit('add-movie', JSON.stringify(this.data))
+      this.toggleAddMovies()
+    },
+    handleSelect (e) {
+      this.movieGenre = e
+      this.selectActive = false
     }
   }
 }
